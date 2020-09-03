0827eb4c1b955118af9bcb9d81a75d479a64167f
gabrielhicks <gabrielhicks@mail.com>
2020-09-03 17:41:57 -0400

    idek

--- README.md
+++ README.md
@@ -4,10 +4,21 @@ You can use this repo to build new Activerecord projects
 
 * You will need to create your own app/models directories
 
-Answer the following questions in the README:
+<!-- Answer the following questions in the README:
 Which method(s) does Active Record create for you?
 - It creates .all method, also attributes are accessible if accessed through User.first.first_name, or using a find query. Was unable to set variable names in my seeds.rb and am confused why rake would populate a table, but not let me access user information via these variables.
 
 Which method(s) did you have to create yourself? Why? -->
 
-tenure, and full name and all in grade. I technically could have accessed these through chainged queries and manipulation, but as far as a one shot method call they are not available
+Add these deliverables:
+- A Student belongs to a Teacher
+- Student#teacher should return the teacher that student belongs to
+- Teacher#students should return a list of all the students that belong to this teacher
+
+Make all necessary changes to satisfy these deliverables: 
+- A Student has many Teachers through GradeLevel
+- A Teacher has many Students through GradeLevel
+You should have a complete seeds file 
+
+Answer the following questions:
+- What changes did you make to your app? Why?
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,29 @@
 Student.destroy_all
 Teacher.destroy_all
+GradeLevel.destroy_all
+
+johnson = Teacher.create(last_name: "Johnson", grade_level: "first", years_of_experience: 10)
+curran = Teacher.create(last_name: "Curran", grade_level: "second", years_of_experience: 20)
+rolf = Teacher.create(last_name: "Rolf", grade_level: "first", years_of_experience: 15)
+van = Teacher.create(last_name: "VanHouten", grade_level: "third", years_of_experience: 5)
 
 gabe = Student.create(first_name: "Gabriel", last_name: "Hicks", grade_level: "first")
 jess = Student.create(first_name: "Jessica", last_name: "Salbert", grade_level: "first")
 jake = Student.create(first_name: "Jacob", last_name: "Fromm", grade_level: "second")
 seyi = Student.create(first_name: "Seyi", last_name: "Kanagui", grade_level: "third")
 
-johnson = Teacher.create(last_name: "Johnson", grade_level: "first", years_of_experience: 10)
-curran = Teacher.create(last_name: "Curran", grade_level: "second", years_of_experience: 20)
-rolf = Teacher.create(last_name: "Rolf", grade_level: "second", years_of_experience: 15)
-van = Teacher.create(last_name: "VanHouten", grade_level: "third", years_of_experience: 5)
+first = GradeLevel.create(student: gabe, teacher: rolf)
+first1 = GradeLevel.create(student: jess, teacher: rolf)
+# second = GradeLevel.create(student: , teacher_id: )
+# third = GradeLevel.create(student: , teacher_id: )
+# fourth = GradeLevel.create(student_id: , teacher_id: )
+
+gabe = Student.first
+jess = Student.second
+jake = Student.third
+seyi = Student.fourth
+
+johnson = Teacher.first
+curran = Teacher.second
+rolf = Teacher.third
+van = Teacher.fourth
